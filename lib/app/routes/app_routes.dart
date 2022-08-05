86e5982b4ee17c8de09fd1ee2b1acaa948db560f
thummar bhargav <thummar0211@gmail.com>
2022-08-05 15:38:11 +0530

    bhargav

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -4,9 +4,11 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
+  static const SINGUP_SCREEN = _Paths.SINGUP_SCREEN;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
+  static const SINGUP_SCREEN = '/singup-screen';
 }
