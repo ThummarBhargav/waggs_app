db417612ab380c63d814af606cb2eff77b3d542a
dell <hiturabadiya007@gmail.com>
2022-08-05 17:47:05 +0530

    Verify OTP Done

--- lib/app/modules/OTP_screen/controllers/o_t_p_screen_controller.dart
+++ lib/app/modules/OTP_screen/controllers/o_t_p_screen_controller.dart
@@ -1,9 +1,10 @@
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:waggs_app/app/constant/ConstantUrl.dart';
 
 class OTPScreenController extends GetxController {
   //TODO: Implement OTPScreenController
 
-  final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
@@ -19,5 +20,26 @@ class OTPScreenController extends GetxController {
     super.onClose();
   }
 
-  void increment() => count.value++;
+  Future<void> otpApi()
+  async {
+    var url = Uri.parse(baseUrl+ApiConstant.verifyOtpUsers);
+    var response = await http.post(url, body: {'email': 'khilanvithani0080@gmail.com',
+      'countryCode': '+91', 'mobile': '8320915951' ,'otp':'098765'});
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+  }
+
+  Future<void> signupApi()
+  async {
+    var url = Uri.parse(baseUrl+ApiConstant.verifyOtpUsers);
+    var response = await http.post(url, body: {
+      'name': '',
+      'mobile': '',
+      'countryCode': '+91',
+      'email': 'khilanvithani0080@gmail.com',
+      'password':''});
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+  }
+
 }
--- lib/app/modules/OTP_screen/views/o_t_p_screen_view.dart
+++ lib/app/modules/OTP_screen/views/o_t_p_screen_view.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:otp_text_field/otp_field.dart';
+import 'package:otp_text_field/style.dart';
+import 'package:waggs_app/app/routes/app_pages.dart';
+
 
 import '../controllers/o_t_p_screen_controller.dart';
 
@@ -9,15 +14,103 @@ class OTPScreenView extends GetView<OTPScreenController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('OTPScreenView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'OTPScreenView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 60,
+                width: 60,
+                margin: EdgeInsets.only(left: 28),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/otp1.png"),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 30,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Text("Enter verification code",
+                  style: GoogleFonts.roboto(
+                    fontSize: 28,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 8,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Text("code was sent to Contact Number",
+                  style: GoogleFonts.roboto(
+                    fontSize: 18,
+                    color: Colors.black45
+                  ),
+                ),
+              )
+            ],
+          ),
+          SizedBox(height: 80,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: OTPTextField(
+                  length: 6,
+                  width: 300,
+                  fieldWidth: 40,
+                  style: TextStyle(
+                      fontSize: 17
+                  ),
+                  textFieldAlignment: MainAxisAlignment.spaceAround,
+                  fieldStyle: FieldStyle.underline,
+                  onCompleted: (pin) {
+                    print("Completed: " + pin);
+                  },
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 80,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              InkWell(
+                onTap: () {
+                  controller.otpApi();
+                  controller.signupApi();
+                  Get.toNamed(Routes.LOGIN_SCREEN);
+                },
+                child: Container(
+                  width: 180,
+                  height: 50,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.all(Radius.circular(80))
+                  ),
+                  child: Text("Verify",
+                    style: GoogleFonts.roboto(
+                      fontSize: 24,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
