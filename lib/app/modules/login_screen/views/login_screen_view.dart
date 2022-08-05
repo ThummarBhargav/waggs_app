fe24c864de5ece08820b2f997dc0984d17120923
khilan <khilanvithani0080@gmail.com>
2022-08-05 18:00:46 +0530

    Login Done

--- lib/app/modules/login_screen/views/login_screen_view.dart
+++ lib/app/modules/login_screen/views/login_screen_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:waggs_app/app/routes/app_pages.dart';
 
 import '../../../constant/sizeConstant.dart';
 import '../../../constant/text_field.dart';
@@ -14,124 +15,148 @@ class LoginScreenView extends GetView<LoginScreenController> {
   );
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('LoginScreenView'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Container(
-            child: Form(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    " LOGIN TO YOUR ACCOUNT",
-                    style: GoogleFonts.roboto(
-                      fontSize: 16,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  sizedBox,
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      padding: EdgeInsets.only(
-                        top: 20,
-                      ),
-                      // width:1000,
-                      child: getTextField(
-                        textEditingController: controller.emailController.value,
-                        borderRadius: 20,
-                        hintText: "Username",
-                        validator: (input) =>
-                            !isNullEmptyOrFalse(input) ? null : "Check your email",
-                        prefixIcon: Padding(
-                          padding: EdgeInsets.all(15),
-                          child: Image(
-                            image: AssetImage("assets/ic_mail.png"),
-                            height: 20,
-                            width: 20,
-                          ),
-                        ),
+    return SafeArea(
+      child: Scaffold(
+          // appBar: AppBar(
+          //   title: const Text('LoginScreenView'),
+          //   centerTitle: true,
+          // ),
+          body: Center(
+            child: Container(
+              child: Form(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      " LOGIN TO YOUR ACCOUNT",
+                      style: GoogleFonts.roboto(
+                        fontSize: 16,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Obx(() {
-                      return Container(
+                    sizedBox,
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
                         padding: EdgeInsets.only(
                           top: 20,
                         ),
-
+                        // width:1000,
                         child: getTextField(
-                          textEditingController:
-                          controller.passController.value,
+                          textEditingController: controller.emailController.value,
                           borderRadius: 20,
-                          hintText: "Password",
-                          validator: (input) => !isNullEmptyOrFalse(input)
-                              ? null
-                              : "Please Enter Password",
-                          textVisible: controller.passwordVisible.value,
+                          hintText: "Username",
+                          validator: (input) =>
+                              !isNullEmptyOrFalse(input) ? null : "Check your email",
                           prefixIcon: Padding(
                             padding: EdgeInsets.all(15),
                             child: Image(
-                              image: AssetImage("assets/ic_lock.png"),
-                              height: 25,
-                              width: 25,
+                              image: AssetImage("assets/ic_mail.png"),
+                              height: 20,
+                              width: 20,
                             ),
                           ),
-                          // onSubmitted: (val) {
-                          //   FocusScope.of(context).unfocus();
-                          //   if (controller.formKey.currentState!
-                          //       .validate()) {
-                          //     // controller.getSingleUserData(
-                          //     //     context: context);
-                          //   }
-                          // },
-                          suffixIcon: InkWell(
-                            onTap: () {
-                              controller.passwordVisible.toggle();
-                            },
-                            child: Padding(
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Obx(() {
+                        return Container(
+                          padding: EdgeInsets.only(
+                            top: 20,
+                          ),
+
+                          child: getTextField(
+                            textEditingController:
+                            controller.passController.value,
+                            borderRadius: 20,
+                            hintText: "Password",
+                            validator: (input) => !isNullEmptyOrFalse(input)
+                                ? null
+                                : "Please Enter Password",
+                            textVisible: controller.passwordVisible.value,
+                            prefixIcon: Padding(
                               padding: EdgeInsets.all(15),
                               child: Image(
-                                image: AssetImage(
-                                    (!controller.passwordVisible.value)
-                                        ? "assets/ic_eye_offf.png"
-                                        : "assets/ic_eye.png"),
-                                height: 20,
-                                width: 20,
+                                image: AssetImage("assets/ic_lock.png"),
+                                height: 25,
+                                width: 25,
+                              ),
+                            ),
+                            // onSubmitted: (val) {
+                            //   FocusScope.of(context).unfocus();
+                            //   if (controller.formKey.currentState!
+                            //       .validate()) {
+                            //     // controller.getSingleUserData(
+                            //     //     context: context);
+                            //   }
+                            // },
+                            suffixIcon: InkWell(
+                              onTap: () {
+                                controller.passwordVisible.toggle();
+                              },
+                              child: Padding(
+                                padding: EdgeInsets.all(15),
+                                child: Image(
+                                  image: AssetImage(
+                                      (!controller.passwordVisible.value)
+                                          ? "assets/ic_eye_offf.png"
+                                          : "assets/ic_eye.png"),
+                                  height: 20,
+                                  width: 20,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      );
-                    }),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                        padding: EdgeInsets.only(
-                          top: 20,
-                        ),
-                        child:
-                            ElevatedButton(onPressed: () {
-
-                                controller.LoginUser();
-
-                            },
-                                child: Text("LOGIN"),
-                            ),
+                        );
+                      }),
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                          padding: EdgeInsets.only(
+                            top: 20,
+                          ),
+                          child:
+                              ElevatedButton(onPressed: () {
+
+                                  controller.LoginUser();
+
+                              },
+                                  child: Text("LOGIN"),
+                              ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Don’t have an account?",
+                          style: GoogleFonts.roboto(
+                            fontSize: 16,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(
+                            textStyle: const TextStyle(fontSize: 20),
+                          ),
+                          onPressed: () {
+                            Get.offAndToNamed(Routes.SINGUP_SCREEN);
+                          },
+                          child: const Text('Sign Up'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ));
+          )),
+    );
   }
 }
