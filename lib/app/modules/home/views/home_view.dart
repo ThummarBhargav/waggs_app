36f021aec5bbedd5b0ea296828fdedf76b899b21
thummar bhargav <thummar0211@gmail.com>
2022-08-08 12:02:23 +0530

    modale

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -6,34 +6,47 @@ import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: SingleChildScrollView(
-          child: Container(
-            height: 700,
-            width: double.infinity,
-            child: Column(
-              children: [
-                  Text("Top Selling Store",style: TextStyle(fontSize:20,color: Colors.orange),),
-                GridView.builder(
-                  itemCount: controller.UserList.length,
-                    physics: NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) {
-                  return Container(
-                    height: 100,
-                    width: 50,
-                    child: Text("${controller.UserList[index].title}"),
-                  );
-                },
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2))
-              ],
-            ),
-          ),
-        )
-      ),
+      child: Obx(() {
+        return Scaffold(
+            body: SingleChildScrollView(
+              child: Expanded(
+                child: Container(
+                  child: Column(
+                    children: [
+                      Text("Top Selling Store",
+                        style: TextStyle(fontSize: 20, color: Colors.orange),),
+                      GridView.builder(
+                          itemCount: controller.UserList.length,
+                          physics: NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            return Column(
+                              children: [
+                                Container(
+                                  height: 100,
+                                  width: 50,
+                                  child: Text("${controller.UserList[index].title}"),
+                                ),
+                                Container(
+                                ),
+                              ],
+                            );
+                          },
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                          crossAxisSpacing: 10,
+                          mainAxisSpacing: 10))
+                    ],
+                  ),
+                ),
+              ),
+            )
+        );
+      }),
     );
   }
 }
