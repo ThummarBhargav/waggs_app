ed10dde5199fb1b6c3509c56874e98c3f19e95a9
Lenovo <argon.krinal@gmail.com>
2022-08-13 09:31:21 +0530

    product detail page

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -20,7 +20,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.HOME;
+  static const INITIAL = Routes.VIEW_PRODUCT;
 
   static final routes = [
     GetPage(
