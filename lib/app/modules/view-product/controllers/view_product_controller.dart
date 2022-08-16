c34a32d674e48cb41b833d7d9bf14ef71394dca6
Khilan <khilanvithani0080@gmail.com>
2022-08-16 15:35:54 +0530

    product page with sub category

--- lib/app/modules/view-product/controllers/view_product_controller.dart
+++ lib/app/modules/view-product/controllers/view_product_controller.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:get/get.dart';
 import 'package:waggs_app/app/Modal/TopSellingStore.dart';
+import 'package:waggs_app/app/constant/ConstantUrl.dart';
+import 'package:http/http.dart' as http;
+import '../../../constant/SizeConstant.dart';
 
 class ViewProductController extends GetxController {
   //TODO: Implement ViewProductController
@@ -8,9 +13,15 @@ class ViewProductController extends GetxController {
   final count = 0.obs;
   RxBool isOpen = false.obs;
   RxBool isOpen1 = false.obs;
+  RxBool hasData = false.obs;
+  RxList<Products0> mainProductList = RxList<Products0>([]);
+  StoreModule storeModule = StoreModule();
+
+
   @override
   void onInit() {
     super.onInit();
+    getData();
   }
 
   @override
@@ -22,5 +33,33 @@ class ViewProductController extends GetxController {
   void onClose() {
     super.onClose();
   }
+  getData() async {
+    hasData.value = false;
+    mainProductList.clear();
+    var URl = Uri.parse(baseUrl +
+        ApiConstant.getAllProductUsers +
+        "/${data.sId}");
+    print(URl);
+    var response;
+    await http.get(URl).then((value) {
+      hasData.value = true;
+      response = value;
+    }).catchError((error) {
+      hasData.value = true;
+    });
+    print(response.body);
+    dynamic result = jsonDecode(response.body);
+    storeModule = StoreModule.fromJson(result);
+    if (!isNullEmptyOrFalse(storeModule.data)) {
+      if (!isNullEmptyOrFalse(storeModule.data!.products)) {
+        storeModule.data!.products!.forEach((element) {
+          mainProductList.add(element);
+        }
+        );
+      }
+    }
+    mainProductList.refresh();
+  }
+
 
 }
