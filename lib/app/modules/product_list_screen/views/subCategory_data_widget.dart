771a19d5c1217831263570d8cde60f9e2e91f941
dell <hiturabadiya007@gmail.com>
2022-08-16 12:26:41 +0530

    Hitarth

--- lib/app/modules/product_list_screen/views/subCategory_data_widget.dart
+++ lib/app/modules/product_list_screen/views/subCategory_data_widget.dart
@@ -5,7 +5,6 @@ import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:waggs_app/app/modules/product_detail_view/views/product_detail_view_view.dart';
-import 'package:waggs_app/app/modules/product_list_screen/views/product_list_screen_view.dart';
 
 import '../../../constant/SizeConstant.dart';
 import '../controllers/product_list_screen_controller.dart';
