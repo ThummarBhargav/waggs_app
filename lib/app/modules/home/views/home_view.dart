aacc946d9c09bdb61e29ddda33a79596039d8e92
thummar bhargav <thummar0211@gmail.com>
2022-08-23 09:41:52 +0530

    addtocart

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1087,12 +1087,6 @@ class HomeView extends GetView<HomeController> {
                                                         data: controller
                                                                 .TopProductlist[
                                                             index]);
-                                                    controller.CartCount();
-                                                    controller.Countlist
-                                                        .refresh();
-                                                    controller.CartProductApi();
-                                                    controller.cartProductList
-                                                        .refresh();
                                                   },
                                                   child: Container(
                                                     width: 130,
