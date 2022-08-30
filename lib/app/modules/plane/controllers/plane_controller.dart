f7a3568446d23040af8b72e67779ad86eb612a1f
Lenovo <argon.krinal@gmail.com>
2022-08-30 15:39:47 +0530

    razor_pay

--- lib/app/modules/plane/controllers/plane_controller.dart
+++ lib/app/modules/plane/controllers/plane_controller.dart
@@ -2,7 +2,9 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
 import 'package:waggs_app/app/Modal/PlanModel.dart';
 import 'package:http/http.dart' as http;
 import 'package:waggs_app/app/Modal/PlanModel.dart';
@@ -17,10 +19,18 @@ class PlaneController extends GetxController {
   PlanModel planModel =  PlanModel();
   RxList<PlanData> planList = RxList<PlanData>([]);
 List respons=[];
+
+  static const platform = const MethodChannel("razorpay_flutter");
+  late Razorpay _razorpay;
   @override
   void onInit() {
     AllPlans();
     super.onInit();
+    _razorpay = Razorpay();
+
+    _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
+    _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR,  _handlePaymentError);
+    _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
   }
 
   @override
@@ -31,6 +41,7 @@ List respons=[];
   @override
   void onClose() {
     super.onClose();
+    _razorpay.clear();
   }
 
   AllPlans() async {
@@ -51,34 +62,79 @@ List respons=[];
   Future<void> PayMent({required PlanData data}) async {
     if((box.read(ArgumentConstant.isUserLogin) == null)){
       Get.toNamed(Routes.LOGIN_SCREEN);
-    }else{
+    }
+    else
+      {
       print('Bearer ${box.read(ArgumentConstant.token)}');
-      try{
-        var url = Uri.parse(baseUrl+ApiConstant.PlansPlayment);
-        var response ;
+      try {
+        var url = Uri.parse(baseUrl + ApiConstant.PlansPlayment);
+        var response;
         await http.post(url, body: {
           'planId': '${data.id}',
           'amount': '${data.amount}'
-        },headers: {
+        }, headers: {
           'Authorization': 'Bearer ${box.read(ArgumentConstant.token)}',
         }
         ).then((value) {
-           response = value;
+          response = value;
         });
         respons.add(response.body);
         print(jsonDecode(response.body).runtimeType);
         print('Response status: ${response.statusCode}');
         print('Response body: ${response.body}');
-        if(response.statusCode==200){
-          Get.snackbar("Success","Payment Done",snackPosition: SnackPosition.BOTTOM,backgroundColor: Colors.green);
+        if (response.statusCode == 200) {
+          var options = {
+            "key": "rzp_live_ILgsfZCZoFIKMb",
+            "amount": data.amount!.toInt() * 100,
+            // Convert Paisa to Rupees
+            "name": "Waggs Payment",
+            "description": "This is a Test Payment",
+            "timeout": "180",
+            "currency": "INR",
+            'send_sms_hash': true,
+            "prefill": {"contact": "8888888888", "email": "test@razorpay.com"},
+            "external": {
+              "wallets": ["paytm"]
+            }
+          };
+          try {
+            _razorpay.open(options);
+          } catch (e) {
+            print(e.toString());
+          }
         }
-        else{
-          Get.snackbar("Error", "Payment Not Done",snackPosition: SnackPosition.BOTTOM,backgroundColor: Colors.orangeAccent);
-        }
+
+        // Get.snackbar("Success","Payment Done",snackPosition: SnackPosition.BOTTOM,backgroundColor: Colors.green);
+
+       // else{
+       //    Get.snackbar("Error", "Payment Not Done",snackPosition: SnackPosition.BOTTOM,backgroundColor: Colors.orangeAccent);
+       //  }
       }catch(e){
         Get.snackbar("Error", e.toString(),snackPosition: SnackPosition.BOTTOM,backgroundColor: Colors.orangeAccent);
       }
     }
   }
+  void _handlePaymentSuccess(PaymentSuccessResponse response) {
+    print('Success Response: $response');
+    Get.snackbar("Success","Payment Done",snackPosition: SnackPosition.BOTTOM,backgroundColor: Colors.green);
+    /*Fluttertoast.showToast(
+        msg: "SUCCESS: " + response.paymentId!,
+        toastLength: Toast.LENGTH_SHORT); */
+  }
+
+  void _handlePaymentError(PaymentFailureResponse response) {
+    print('Error Response: $response');
+    /* Fluttertoast.showToast(
+        msg: "ERROR: " + response.code.toString() + " - " + response.message!,
+        toastLength: Toast.LENGTH_SHORT); */
+  }
+
+  void _handleExternalWallet(ExternalWalletResponse response) {
+    print('External SDK Response: $response');
+    /* Fluttertoast.showToast(
+        msg: "EXTERNAL_WALLET: " + response.walletName!,
+        toastLength: Toast.LENGTH_SHORT); */
+  }
 
 }
+
--- lib/app/modules/plane/views/plane_view.dart
+++ lib/app/modules/plane/views/plane_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
