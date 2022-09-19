81cee9e3ee6d743be557a8c344147b6d0f415c42
Lenovo <argon.krinal@gmail.com>
2022-09-19 14:16:09 +0530

    bug fix

--- lib/app/modules/date_time_appoiment/views/date_time_appoiment_view.dart
+++ lib/app/modules/date_time_appoiment/views/date_time_appoiment_view.dart
@@ -86,7 +86,7 @@ class DateTimeAppoimentView extends GetWidget<DateTimeAppoimentController> {
                                     size: 25,
                                     color: Colors.grey[500],
                                   )),
-                              controller.count1.data == 0
+                              (controller.count1.data == 0||controller.count1.data==null)
                                   ? Container()
                                   : Positioned(
                                       left: 22,
